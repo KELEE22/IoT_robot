6fdb046578374170e8884766420c1a856ebefdba
Rizzzaki <imkelee@gmail.com>
2023-06-23 05:15:11 -0700

    opencv

---
+++ class15.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+img= np.zeros(shape=(512,512,3), dtype=np.uint8) +255
+
+pts1 = np.array([[100,140], [200,100], [200,200], [100,200]])   #파이참 alt+N 
+pts2 = np.array([[300,200],[400,100],[400,200]])
+
+cv2.polylines(img, [pts1, pts2], isClosed=True, color=(255,0,0), thickness=3)
+
+
+cv2.imshow('img', img)
+cv2.waitKey()
+cv2.destroyAllWindows()
